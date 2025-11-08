e6558e5edf131a3ac3d786faedd9e9e84769ef3e
SergeyYakushevskiy <yakushevskiy.s.s@mail.ru>
2025-11-08 19:10:13 +0300

    completed lab1

---
+++ lab1/src/main/java/dstu/csae/pafis/App.java
@@ -0,0 +1,192 @@
+package dstu.csae.pafis;
+
+import dstu.csae.pafis.cipher.RSA;
+import dstu.csae.pafis.converter.StringConverter;
+import dstu.csae.pafis.utils.NumberOperator;
+
+import java.io.*;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class App {
+
+    public static final Scanner SCANNER = new Scanner(System.in);
+    public static final int RSA_BIT_COUNT = 512;
+
+    public static final String DEFAULT_PATH = System.getProperty("user.home");
+
+    public static final String[] KEY_INIT_MODES = {
+            "1. Генерация пары ключей",
+            "2. Задание пары ключей вручную"
+    };
+    public static final String[] TEXT_INPUT_MODES = {
+            "1. Ввод текста вручную",
+            "2. Считать текст из файла"
+    };
+    public static final String[] APP_MODES = {
+            "1. Зашифровать текст",
+            "2. Расшифровать текст"
+    };
+
+    public static void main(String[] args) {
+        System.out.println("""
+                === ЛАБОРАТОРНАЯ РАБОТА № 1 ===
+                Реализация шифрования алгоритмом RSA
+                """);
+        int keyInitMode = getMode(KEY_INIT_MODES);
+        RSA rsa = null;
+        do {
+            switch (keyInitMode) {
+                case 1 -> {
+                    BigInteger p = NumberOperator.nextPrimeBigInt(RSA_BIT_COUNT);
+                    BigInteger q = NumberOperator.nextPrimeBigInt(RSA_BIT_COUNT);
+                    BigInteger m = p.subtract(BigInteger.ONE).multiply(q.subtract(BigInteger.ONE));
+                    BigInteger e = NumberOperator.nextPrimeBigInt(RSA_BIT_COUNT, BigInteger.TWO, m);
+                    rsa = new RSA(p, q, e);
+                    BigInteger d = rsa.getD();
+                    System.out.println(rsa);
+                    saveData(DEFAULT_PATH + "/public.key", e.toString(2), p.toString(2), q.toString(2));
+                    saveData(DEFAULT_PATH + "/private.key", d.toString(2), p.toString(2), q.toString(2));
+                }
+                case 2 -> {
+                    String publicKeyPath = "";
+                    String privateKeyPath = "";
+                    do {
+                        System.out.print("Введите путь к файлу с открытым ключом: ");
+                        publicKeyPath = SCANNER.next();
+                    } while (publicKeyPath.isBlank());
+                    do {
+                        System.out.print("Введите путь к файлу с закрытым ключом: ");
+                        privateKeyPath = SCANNER.next();
+                    } while (privateKeyPath.isBlank());
+                    List<BigInteger> values = initData(publicKeyPath);
+                    BigInteger e = values.get(0);
+                    BigInteger p = values.get(1);
+                    BigInteger q = values.get(2);
+                    values = initData(privateKeyPath);
+                    BigInteger d = values.get(0);
+                    rsa = new RSA(p, q, e);
+                    System.out.println(rsa);
+                }
+                default -> System.out.println("Ошибка при выборе режима задания алгоритма");
+            }
+        }while (rsa == null);
+
+        int inputTextMode = getMode(TEXT_INPUT_MODES);
+        String text;
+        if (SCANNER.hasNextLine()) {
+            SCANNER.nextLine();
+        }
+        do{
+            switch (inputTextMode) {
+                case 1 -> {
+                    System.out.print("Введите текст: ");
+                    text = SCANNER.nextLine();
+                    if(text.isBlank()){
+                        System.out.println("Пустая строка. Повторите попытку.");
+                    }
+                    saveData(DEFAULT_PATH + "/" + "text.txt", text);
+                }
+                case 2 -> {
+                    System.out.print("Введите путь к тексту: ");
+                    String textPath = SCANNER.nextLine();
+                    text = getText(textPath);
+                    System.out.printf("Текст: %s\n", text);
+                }
+                default -> {
+                    System.out.println("Ошибка при выборе режима ввода текста");
+                    text = "";
+                }
+            }
+        }while (text.isBlank());
+
+        int appMode = getMode(APP_MODES);
+        if(SCANNER.hasNextLine()){
+            SCANNER.nextLine();
+        }
+        switch (appMode){
+            case 1 -> {
+                ArrayList<BigInteger> converted = StringConverter.toBigInt(text);
+                ArrayList<BigInteger> encryptedList = new ArrayList<>(
+                        converted.stream()
+                        .map(rsa::encrypt)
+                        .toList()
+                );
+                String encrypted = StringConverter.toString(encryptedList);
+                System.out.printf("Шифртекст: %s\n", encrypted);
+                saveData(DEFAULT_PATH + "/encryptedtext.txt",
+                        encryptedList.stream()
+                        .map(BigInteger::toString)
+                        .collect(Collectors.joining(", ")
+                        ));
+            }
+            case 2 -> {
+                ArrayList<BigInteger> converted = StringConverter.toBigInt(text.split(", "));
+                ArrayList<BigInteger> decryptedList = new ArrayList<>(
+                    converted.stream()
+                            .map(rsa::decrypt)
+                            .toList()
+                );
+                String decrypted = StringConverter.toString(decryptedList);
+                System.out.printf("Открытый текст: %s\n", decrypted);
+                saveData(DEFAULT_PATH + "/plaintext.txt", decrypted);
+            }
+        }
+    }
+
+    public static int getMode(String ... options){
+        if(options.length < 2){
+            throw new IllegalArgumentException("Число режимов работы должно быть не меньше 2");
+        }
+        Arrays.stream(options).forEach(System.out::println);
+        int mode = 0;
+        do{
+            try{
+                System.out.print("Введите номер необходимого режима работы: ");
+                 mode = SCANNER.nextInt();
+            }catch (NumberFormatException ex){
+                System.out.println("Повторите попытку.");
+            }
+        }while((0 < mode - 1) && (mode - 1 >= options.length));
+        return mode;
+    }
+
+    public static List<BigInteger> initData(String path){
+        List<BigInteger> values = new ArrayList<>();
+        try(BufferedReader reader = new BufferedReader(new FileReader(path))){
+            String line;
+            while((line = reader.readLine()) != null){
+                BigInteger value = new BigInteger(line, 2);
+                values.add(value);
+            }
+        } catch (IOException e) {
+            System.out.println("Ошибка: " + e.getMessage());
+        }
+        return values;
+    }
+
+    public static void saveData(String path, String ... values){
+        try(FileWriter writer = new FileWriter(path)){
+            String writable = String.join("\n", values);
+            writer.write(writable);
+            writer.flush();
+            System.out.println("Данные успешно сохранены в файл: " + path);
+        }catch (IOException ex){
+            System.out.println("Ошибка при сохранении данных: " + ex.getMessage());
+        }
+    }
+
+    public static String getText(String path){
+        List<String> lines = new ArrayList<>();
+        try(BufferedReader reader = new BufferedReader(new FileReader(path))){
+            lines = reader.lines().toList();
+        } catch (IOException e) {
+            System.out.println("Ошибка: " + e.getMessage());
+        }
+        return String.join("\n", lines);
+    }
+}
--- lab1/src/main/java/dstu/csae/pafis/Main.java
+++ lab1/src/main/java/dstu/csae/pafis/Main.java
@@ -1,7 +1,141 @@
 package dstu.csae.pafis;
 
+import dstu.csae.pafis.cipher.RSA;
+import dstu.csae.pafis.converter.StringConverter;
+import dstu.csae.topg.prime.PrimeNumberOperator;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
 public class Main {
+    public static final String RETRY = "Повторите попытку ввода";
+    public static final int MIN_KEY_BIT_COUNT = 1024;
+    public static final int MIN_PRIME_BIT_COUNT = 1024;
+    public static final String DEFAULT_USER_PATH = System.getProperty("user.home");
 
 
+    public static RSA rsa;
+
+    public static void main(String[] args) {
+        String QUERY = "1. Генерация модуля n и числа e\n" +
+                "2. Ввод чисел n и e вручную\n" +
+                "Введите номер выбранного режима: ";
+        boolean mode = setMode(QUERY);
+        BigInteger p = !mode ? inputNumber("p") : generatePrime();
+        BigInteger q = !mode ? inputNumber("q") : generatePrime();
+        BigInteger n = p.multiply(q);
+        BigInteger m = p.subtract(BigInteger.ONE).multiply(q.subtract(BigInteger.ONE));
+        System.out.println("p = " + p);
+        System.out.println("q = " + q);
+        System.out.println("n = p * q = " + n);
+        System.out.println("φ(n) = " + m);
+        BigInteger e = !mode ?
+                inputNumber("e", BigInteger.TWO, m) :
+                generatePrime(BigInteger.TWO, m);
+        RSA.setMinKeyBitCount(MIN_KEY_BIT_COUNT);
+        rsa = new RSA(p, q, e);
+        System.out.println(rsa);
+        QUERY = "1. Зашифровать\n" +
+                "2. Расшифровать\n" +
+                "Введите номер нужной операции: ";
+        boolean state = setMode(QUERY);
+        String text = getText();
+        String out = state ? "Шифртекст: " : "Открытый текст: ";
+        ArrayList<BigInteger> bigIntegers = state ?
+                StringConverter.toBigInt(text) :
+                StringConverter.toBigInt(text.split(", "));
+        bigIntegers = state ? encrypt(bigIntegers) : decrypt(bigIntegers);
+        out += StringConverter.toString(bigIntegers);
+        System.out.println(out);
+        System.out.println(bigIntegers.stream()
+                .map(BigInteger::toString)
+                .collect(Collectors.joining(", ")));
+    }
+
+    public static String getText(){
+        String text = "";
+        Scanner scanner = new Scanner(System.in);
+        while (text.isBlank()){
+            System.out.print("Введите текст: ");
+            text = scanner.nextLine();
+        }
+        return text;
+    }
+
+    public static boolean setMode(String query){
+        Boolean mode = null;
+        Scanner scanner = new Scanner(System.in);
+        while (mode == null){
+            System.out.print(query);
+            try{
+                int m = Integer.parseInt(scanner.nextLine());
+                if(m < 1 || m > 2){
+                    System.out.println(RETRY);
+                    continue;
+                }
+                mode = m == 1;
+            }catch (NumberFormatException ex){
+                System.out.println(ex.getMessage());
+                System.out.println(RETRY);
+            }
+        }
+        return mode;
+    }
+
+    public static ArrayList<BigInteger> encrypt(ArrayList<BigInteger> plaintext){
+        return plaintext.stream()
+                .map(rsa::encrypt)
+                .collect(Collectors.toCollection(ArrayList::new));
+    }
+
+    public static ArrayList<BigInteger> decrypt(ArrayList<BigInteger> ciphertext){
+        return ciphertext.stream()
+                .map(rsa::decrypt)
+                .collect(Collectors.toCollection(ArrayList::new));
+    }
+
+    public static BigInteger generatePrime(BigInteger origin, BigInteger bound){
+        return PrimeNumberOperator.nextBigInt(MIN_KEY_BIT_COUNT, origin, bound);
+    }
+
+    public static BigInteger generatePrime(){
+        return PrimeNumberOperator.nextBigInt(MIN_PRIME_BIT_COUNT);
+    }
+
+    public static BigInteger inputNumber(String numberName, BigInteger origin, BigInteger bound){
+        BigInteger out = null;
+        while (out == null){
+            out = inputNumber(numberName);
+            if(out.compareTo(origin) < 0 || out.compareTo(bound) >= 0){
+                System.out.println("Значение не входит в диапазон допустимых параметров");
+                System.out.println(RETRY);
+                out = null;
+            }
+        }
+        return out;
+    }
+
+    public static BigInteger inputNumber(String numberName){
+        String QUERY = String.format("Введите значение %s: ", numberName);
+        Scanner scanner = new Scanner(System.in);
+        BigInteger number = null;
+        while (number == null ){
+            System.out.print(QUERY);
+            try{
+                number = new BigInteger(scanner.nextLine());
+                if(!PrimeNumberOperator.isProbablePrime(number)){
+                    System.out.println("Число не является простым");
+                    System.out.println(RETRY);
+                    number = null;
+                }
+            }catch (Exception ex){
+                System.out.println(ex.getMessage());
+                System.out.println(RETRY);
+            }
+        }
+        return number;
+    }
 
 }
